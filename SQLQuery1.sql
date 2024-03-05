5b0a53b22dcd22ddb3d7ff118b3e837e7a1f23bd
ARVUTI\andri <andri.klas@hotmail.com>
2024-03-05 09:56:20 +0200

    no message

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,16 +1,16 @@
 --loome db
-create database Targe23SQL
+create database TARge23SQL
 
---db valimine
-use Targe23SQL
+-- db valimine
+use TARge23SQL
 
 --db kustutamine
-drop database Targe23SQL
+drop database TARge23SQL
 
 -- tabeli loomine
 create table Gender
 (
-ID int not null primary key,
+Id int not null primary key,
 Gender nvarchar(10) not null
 )
 
@@ -18,18 +18,18 @@ Gender nvarchar(10) not null
 insert into Gender (Id, Gender)
 values (1, 'Male'),
 (2, 'Female'),
-(3, 'Unkown')
+(3, 'Unknown')
 
---vaatame tabeli sisu
+-- vaatame tabeli sisu
 select * from Gender
 
 --loome uue tabeli
 create table Person
 (
-ID int not null primary key,
+Id int not null primary key,
 Name nvarchar(30),
 Email nvarchar(30),
-GenderID int
+GenderId int
 )
 
 --andmete sisestamine
@@ -38,116 +38,119 @@ insert into Person (Id, Name, Email, GenderId) values
 (2, 'Wonderwoman', 'w@w.com', 2),
 (3, 'Batman', 'b@b.com', 1),
 (4, 'Aquaman', 'a@a.com', 1),
-(5, 'Catman', 'c@c.com', 2),
+(5, 'Catwoman', 'c@c.com', 2),
 (6, 'Antman', 'ant"ant.com', 1),
 (7, NULL, NULL, 3)
 
-
---vaadake PErson tabeli andmeid
+--vaadake Person tabeli andmeid
 select * from Person
 
---võõrvõtme ühenduse loomine kahe tabeli vahel
-alter table Person add constraint tblPerson_GenderID_FK
+-- võõrvõtme ühenduse loomine kahe tabeli vahel
+alter table Person add constraint tblPerson_GenderId_FK
 foreign key (GenderId) references Gender(Id)
 
 --kui sisestad uue rea andmeid ja ei ole sisestanud GenderId-d
---alla väärtust, siis see automaatselt sisestab selle reale
---väärtuse 3 e Unknown
+--alla väärtust, siis see automaatselt sisestab sellele reale
+--väärtuse 3 e unknown
 alter table Person
 add constraint DF_persons_GenderId
 default 3 for GenderId
 
---sisestame andmed 
+--sisestame andmed
+--
 insert into Person (Id, Name, Email)
-values(9, 'Ironman','i@i.com')
+values (9, 'Ironman', 'i@i.com')
 
 select * from Person
 
+--lisame uue veeru
 alter table Person
-Add Age Int;
---lisame nr piirangu vanuse sisestamisel
+add Age nvarchar(10)
 
-
+-- lisame nr piirangu vanuse sisestamisel
 alter table Person
-add constraint CK_Person_Age check (age > 0 and Age < 155)
+add constraint CK_Person_Age check (Age > 0 and Age < 155)
 
-insert into Person (Id, Name, Email)
+-- sisestame uue rea andmeid
+insert into Person (Id, Name, Email, GenderId, Age)
 values (10, 'Kalevipoeg', 'k@k.com', 1, 30)
 
 --muudame koodiga andmeid
 update Person
-set Age = 50
-where Id = 5
+set Age = 35
+where Id = 9
 
 select * from Person
 
+--sisestame muutuja City nvarchar(50)
 alter table Person
 add City nvarchar(50)
 
 --sisestame City veergu andmeid
 update Person
 set City = 'Kaljuvald'
-where ID = 10
+where Id = 10
 
+select * from Person
 
---kõik kes Gothami linnas
+-- kõik, kes elavad Gothami linnas
 select * from Person where City = 'Gotham'
-
--- kõik kes ei ela Gothamis
+--kõik, kes ei ela Gothamis
+select * from Person where City <> 'Gotham'
+--kõik, kes ei ela Gothamis
 select * from Person where City != 'Gotham'
 
---näitab teatud vanusega inimesi
-select * from Person where Age = 12 or Age = 50 or Age = 19
+-- näitab teatud vanusega inimesi
+select * from Person where Age = 120 or Age = 50 or Age = 19
 select * from Person where Age in (120, 50, 19)
+
+-- näitab teatud vanusevahemikus olevaid inimesi
+-- ka 22 ja 31 aastaseid
 select * from Person where Age between 22 and 31
 
-
---wildcard e näitab kõik g-tähega algavad
-linnad
-select * from Person where City like 'g%'
-
-
+-- wildcard e n'itab kõik n-tähega linnad
+-- n-t'hega algavad linnad
+select * from Person where City like 'n%'
 --kõik emailid, kus on @-märk sees
 select * from Person where Email like '%@%'
 
---näitab emaile , kus ei ole @-märki
+-- näitab Emaile, kus ei ole @-m'rki sees
 select * from Person where Email not like '%@%'
 
---näitab kellel on emailis ees ja peale @märki ainult üks täht
-
-
+--näitab, kellel on emailis ees ja peale @-m'rki ainult [ks t'ht
+select * from Person where Email like '_@_.com'
 
 update Person
 set Email = 'bat@bat.com'
 where Id = 3
 
-
---kõik kellel nimes ei ole esimene täht W, A, C
+-- kõik, kellel nimes ei ole esimene täht W, A, C
 select * from Person where name like '[^WAC]%'
 select * from Person
 
+--kes elavad Gothamis ja New York-s
+select * from Person where (City = 'Gotham' or City = 'New York')
 
---kes elavad Gothamis ja New Yorgis
-select * from Person where (city = 'Gotham' or city = 'New York')
-
-
---kõik kes elavad linnades ja on vanemad kui 29
-select * from Person where (city = 'Gotham' or city = 'New York')
+-- kõik, kes elavad välja toodud linnades ja on vanemad, kui
+-- 29
+select * from Person where (City = 'Gotham' or City = 'New York')
 and Age >= 30
 
-
---kuvab tähestikulises järjekorras inimesi ja võtab aluseks nime
+-- kuvab tähestikulises järjekorras inimesi ja võtab 
+-- aluseks nime
+select * from Person order by Name
+-- kuvab vastupidises järjestuses
 select * from Person order by Name desc
 
---võtab 3 esimest rida
+--võtab kolm esimest rida
 select top 3 * from Person
 
---võtab kolm esimest aga tabeli järjestus on Age ja siis Name
+-- kolm esimest, aga tabeli järjestus on Age ja siis Name
 select * from Person
 select top 3 Age, Name from Person
 
---näita esimesed 50% tabelist
-select top 50 percent *  from Person
+-- näita esimene 50% tabeli sisust
+select top 50 percent * from Person
 
---Järjestab vanuse järgi isikud
-select * from person order by cast(Age as int) desc
+--järjestab vanuse järgi isikud
+select * from Person order by cast(Age as int)
